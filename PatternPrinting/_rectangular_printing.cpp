5360d4f01c3b3d50e48363e5d3a6f416090af0b9
Atul Tiwari <tiwariatul9526@gmail.com>
2022-11-05 20:40:29 +0530

    rectangular printing

---
+++ PatternPrinting/_rectangular_printing.cpp
@@ -0,0 +1,29 @@
+
+/*
+   Rectangular Printing
+   *****
+   *****
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int n, m;
+  cin >> n;
+  cin >> m;
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= m; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
